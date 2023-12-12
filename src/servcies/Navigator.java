33bf63367d2ed4a2d666eb2e0ba08aed35f18d52
mayar <100476119+mayar1711@users.noreply.github.com>
2023-12-12 09:24:45 +0200

    Select Level

--- src/servcies/Navigator.java
+++ src/servcies/Navigator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package serves;
+package servcies;
 
 import java.awt.event.MouseEvent;
 import javafx.event.ActionEvent;
---
+++ src/tictactoeclient/selectLevelBase.java
@@ -0,0 +1,68 @@
+package tictactoeclient;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public  class selectLevelBase extends AnchorPane {
+
+    protected final ImageView EeasyImage;
+    protected final Button EasyButton;
+    protected final ImageView HardImage;
+    protected final Button HardButton;
+
+    public selectLevelBase() {
+
+        EeasyImage = new ImageView();
+        EasyButton = new Button();
+        HardImage = new ImageView();
+        HardButton = new Button();
+
+        setId("AnchorPane");
+        setPrefHeight(600.0);
+        setPrefWidth(800.0);
+        setStyle("-fx-background-color: #34365C;");
+
+        EeasyImage.setFitHeight(200.0);
+        EeasyImage.setFitWidth(200.0);
+        EeasyImage.setLayoutX(105.0);
+        EeasyImage.setLayoutY(123.0);
+        EeasyImage.setPickOnBounds(true);
+        EeasyImage.setPreserveRatio(true);
+        EeasyImage.setRotate(15.0);
+        EeasyImage.setImage(new Image(getClass().getResource("xoImg.png").toExternalForm()));
+
+        EasyButton.setLayoutX(150.0);
+        EasyButton.setLayoutY(392.0);
+        EasyButton.setMnemonicParsing(false);
+        EasyButton.setPrefHeight(40.0);
+        EasyButton.setPrefWidth(120.0);
+        EasyButton.setStyle("-fx-background-color: #8B91B5;");
+        EasyButton.setText("Easy");
+        EasyButton.setTextFill(javafx.scene.paint.Color.WHITE);
+
+        HardImage.setFitHeight(200.0);
+        HardImage.setFitWidth(200.0);
+        HardImage.setLayoutX(496.0);
+        HardImage.setLayoutY(123.0);
+        HardImage.setPickOnBounds(true);
+        HardImage.setPreserveRatio(true);
+        HardImage.setImage(new Image(getClass().getResource("Controllers/hard.png").toExternalForm()));
+
+        HardButton.setLayoutX(551.0);
+        HardButton.setLayoutY(392.0);
+        HardButton.setMnemonicParsing(false);
+        HardButton.setPrefHeight(40.0);
+        HardButton.setPrefWidth(120.0);
+        HardButton.setStyle("-fx-background-color: #8B91B5;");
+        HardButton.setText("Hard");
+        HardButton.setTextFill(javafx.scene.paint.Color.WHITE);
+
+        getChildren().add(EeasyImage);
+        getChildren().add(EasyButton);
+        getChildren().add(HardImage);
+        getChildren().add(HardButton);
+
+    }
+}
