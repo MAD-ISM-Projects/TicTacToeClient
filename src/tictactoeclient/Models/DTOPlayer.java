017afdc091081cf0c02cb1a5a12ab4b408830f89
Husayn <hessein.abdulaziz2001@gmail.com>
2023-12-12 10:12:52 +0200

    adding enum and DTO

---
+++ src/tictactoeclient/Models/DTOPlayer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoeclient.Models;
+
+import tictactoeclient.Enum.Mark;
+
+/**
+ *
+ * @author NEW LAP
+ */
+public class DTOPlayer {
+    private String playerName;
+    private Mark mark;
+    
+    public DTOPlayer(){
+        
+    }
+    public DTOPlayer(String playerName,Mark mark){
+        this.playerName=playerName;
+        this.mark=mark;
+    }
+    public void setName(String playerName){
+        this.playerName=playerName;
+    }
+    public void setMark(Mark mark){
+       this.mark=mark;
+    }
+    public String getName(){
+        return playerName;
+    }
+    public Mark getMark(){
+        return mark;
+    }
+
+
+}
